09f65622880070f255c8845d859170556344996b
Blez2 <40924272+Blez2@users.noreply.github.com>
2018-07-07 14:54:38 -0700

    Add files via upload

---
+++ accountant.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class accountant{
+public interface accountant{
+  public int getHours(int hours);
+  public double getSalary(double salary);
+  public String occupationName(String name);
+
+}
